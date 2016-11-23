19cef4ee660e4bdf6f44e3aac9ef67c61267cc95
eddydn <eddydn@gmail.com>
2016-11-23 16:39:32 +0700

    First Commit
    First Commit

---
+++ MainActivity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace AndroidScrollView
+{
+    [Activity(Label = "AndroidScrollView", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+          
+        }
+    }
+}
+
